40a8e17b651fcfe4fc64dc19620b48eb6d5ac91f
Jonathan Vinicius Braz Silva <jonathanvinicius211@gmail.com>
2025-03-13 20:17:46 -0300

    feat:

---
+++ apps/core/src/modules/vehicle/dtos/patch-vehicle.dto.ts
@@ -0,0 +1,28 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class PatchVehicleDto {
+  @IsString()
+  @ApiProperty()
+  plate: string;
+
+  @IsString()
+  @ApiProperty()
+  chassis: string;
+
+  @IsString()
+  @ApiProperty()
+  renavam: string;
+
+  @IsString()
+  @ApiProperty()
+  model: string;
+
+  @IsString()
+  @ApiProperty()
+  brand: string;
+
+  @IsString()
+  @ApiProperty()
+  vehicle_year: Date;
+}
