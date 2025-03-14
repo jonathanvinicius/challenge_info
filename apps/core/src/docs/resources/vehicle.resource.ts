a9489f247688170bee2292c0e68ee094b937be21
Jonathan Vinicius Braz Silva <jonathanvinicius211@gmail.com>
2025-03-14 10:11:38 -0300

    refactor: :recycle: update path application

--- apps/core/src/docs/resources/vehicle.resource.ts
+++ apps/core/src/docs/resources/vehicle.resource.ts
@@ -2,6 +2,6 @@ import { SwaggerResource } from '@app/domain/common/swagger.resource';
 
 export const RESOURCE_VEHICLE: SwaggerResource = {
   tag: 'Vehicle',
-  route: 'vehicle',
+  route: 'vehicles',
   description: '',
 };
