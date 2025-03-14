584a908ec50d3165d1850214a4d378e4a855b565
Jonathan Vinicius Braz Silva <jonathanvinicius211@gmail.com>
2025-03-14 10:03:41 -0300

    refactor: :recycle: add vehicle data validation

--- apps/core/src/modules/vehicle/dtos/post-vehicle.dto.ts
+++ apps/core/src/modules/vehicle/dtos/post-vehicle.dto.ts
@@ -1,16 +1,23 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString } from 'class-validator';
+import { Transform } from 'class-transformer';
+import { IsString, Length } from 'class-validator';
 
 export class PostVehicleDto {
   @IsString()
+  @Transform(({ value }) => value.replace(/-/g, ''))
+  @Length(7, 7, { message: 'Plate must have 7 characters' })
   @ApiProperty()
   plate: string;
 
   @IsString()
+  @Transform(({ value }) => value.replace(/-/g, ''))
+  @Length(17, 17, { message: 'Chassis must have 17 characters' })
   @ApiProperty()
   chassis: string;
 
   @IsString()
+  @Transform(({ value }) => value.replace(/-/g, ''))
+  @Length(11, 11, { message: 'Renavam must have 11 characters' })
   @ApiProperty()
   renavam: string;
 
